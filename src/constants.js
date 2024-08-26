89d0df3dd2a97ff9464f7045e459edcec9effb3c
varsha dahare <varsha.dahare@ziroh.com>
2024-08-26 16:54:18 +0530

    added images url

---
+++ src/constants.js
@@ -0,0 +1,7 @@
+export const data = [
+    "https://i.pinimg.com/564x/39/11/6c/39116c247669762f4ce72be4ce2b862e.jpg",
+    "https://c4.wallpaperflare.com/wallpaper/297/22/531/lake-blue-moonlight-moon-wallpaper-thumb.jpg",
+    "https://t3.ftcdn.net/jpg/05/85/86/44/360_F_585864419_kgIYUcDQ0yiLOCo1aRjeu7kRxndcoitz.jpg",
+    "https://wallpapers.com/images/featured/nature-2ygv7ssy2k0lxlzu.jpg",
+    "https://img.freepik.com/free-photo/painting-mountain-lake-with-mountain-background_188544-9126.jpg"
+]
